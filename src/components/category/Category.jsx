7dfca84ffe6987542c722ef8e6623108844ba65a
Vipul <vipuldamrepro26@gmail.com>
2024-10-28 20:42:05 +0530

    category section done with responsiveness

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import './category.css';
 
 const Category = () => {
   return (
-    <div>Category</div>
+    <div className='category'>
+      <div className="category-section">
+        <div className="category-section1">
+          <div className="category-section1-items">
+            <p>Movies</p>
+            <p>Stream</p>
+            <p>Events</p>
+            <p>Plays</p>
+            <p>Sports</p>
+            <p>Activities</p>
+          </div>
+        </div>
+        <div className="category-section2">
+          <div className="category-section2-items">
+            <p>ListYourShow</p>
+            <p>Corporates</p>
+            <p>Offers</p>
+            <p>Gift Cards</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
