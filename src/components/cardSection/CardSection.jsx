2adaf9481cb61088fec73c639ee71d15dabe1319
Vipul <vipuldamrepro26@gmail.com>
2024-11-03 11:24:52 +0530

    cards-section done with resposiveness

--- src/components/cardSection/CardSection.jsx
+++ src/components/cardSection/CardSection.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import fetchData from '../../utilities/fetchData';
+import './cardsection.css';
 
 import { FaAngleRight } from "react-icons/fa6";
 import "slick-carousel/slick/slick.css";
@@ -18,7 +19,34 @@ const CardSection = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 5,
-    slidesToScroll: 1,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 1024, // Screen width of 1024px or less
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768, // Screen width of 768px or less (Tablet)
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 480, // Screen width of 480px or less (Mobile)
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
 
@@ -28,6 +56,8 @@ const CardSection = () => {
       try {
         const res = await fetchData('movie');
         if (res && res.data && res.data.Search) {
+          console.log(res.data.Search);
+          
           setData(res.data.Search);
         }
       } catch (error) {
@@ -56,8 +86,12 @@ const CardSection = () => {
               data.length > 0 ? (
                 data.map((card) => (
                   <div className="card" key={card.imdbID}>
-                    <p>{card.Title}</p>
+                    {/* <p>{card.Title}</p> */}
+                    <div className="img">
                     <img src={card.Poster} alt={card.Title} />
+                    </div>
+                    <h3>{card.Title}</h3>
+                    <p>{card.Type}</p>
                   </div>
                 ))
               ) : (
